c8c4d3dbdf3fbf2121a1a92447c78bbbcdc59c95
zhengqirui <201250229@smail.nju.edu.cn>
2024-11-01 15:44:10 +0800

    change

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import numpy as np
 import streamlit as st
 import joblib
+import pandas as pd
 # 定义模型文件的完整路径
 model_path = 'model/rf_model.pkl'
 
@@ -21,7 +23,7 @@ TBIL = st.number_input('TBIL (μmol/L)', min_value=1.0, max_value=100.0, step=0.
 SCr = st.number_input('SCr (μmol/L)', min_value=0.0, max_value=300.0, step=0.1)
 pre_shock = st.selectbox('pre_shock', options=["Yes", "No"])
 area_of_burn = st.number_input('area_of_burn (%)', min_value=10, max_value=100, step=1)
-three = st.number_input('Ⅲ (%)', min_value=0, max_value=80, step=1)
+three = st.number_input('Ⅲ (%)', min_value=0, max_value=100, step=1)
 inhalation_damage = st.selectbox('inhalation_damage', options=["Yes", "No"])
 ALB = st.number_input('ALB (g/L)', min_value=5.0, max_value=50.0, step=0.1)
 
@@ -49,10 +51,10 @@ else:
 # 定义应用程序行为
 if st.button('Predict'):
     # 将用户输入的特征转换为模型所需的输入格式
-    input_features = [[SOFA, new_onset_shock, MDR, los_of_ICU, TBIL, SCr, pre_shock, area_of_burn, three, inhalation_damage, ALB]]
-
+    input_features = np.array([[area_of_burn, three, pre_shock, inhalation_damage, los_of_ICU, new_onset_shock, MDR, TBIL, ALB, SCr, SOFA]])
+    df = pd.DataFrame(input_features, columns=["area_of_burn", "III", "pre_shock", "Inhalation_Damage", "LOS_of_ICU", "new_onset_shock", "MDR", "TBIL", "ALB", "SCr", "SOFA"])
     # 使用加载的模型进行预测
-    prediction = loaded_model.predict_proba(input_features)
+    prediction = loaded_model.predict_proba(df)
     print(prediction)
 
     # 显示预测结果
