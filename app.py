b6293a999387e411b3b017161ed5b9f63c619378
zhengqirui <201250229@smail.nju.edu.cn>
2024-11-01 14:22:56 +0800

    debug

--- app.py
+++ app.py
@@ -12,18 +12,35 @@ st.title('Random Forest Model Deployment')
 st.write('Enter some input features to make predictions:')
 
 # 创建输入框用于用户输入特征
-new_onset_shock = st.selectbox('new onset shock ', options=[0, 1])
-DBIL = st.number_input('DBIL', min_value=0.00, max_value=50.00, step=0.01)
+new_onset_shock = st.selectbox('new_onset_shock', options=["Yes", "No"])
+DBIL = st.number_input('DBIL (μmol/L)', min_value=0.00, max_value=50.00, step=0.01)
 SOFA = st.number_input('SOFA', min_value=0, max_value=9, step=1)
-MDR = st.selectbox('MDR', options=[0, 1])
+MDR = st.selectbox('MDR', options=["Yes", "No"])
 los_of_ICU = st.number_input('Los_of_ICU', min_value=1, max_value=150, step=1)
-TBIL = st.number_input('TBIL', min_value=1.0, max_value=100.0, step=0.1)
-SCr = st.number_input('SCr', min_value=0.0, max_value=300.0, step=0.1)
-pre_shock = st.selectbox('pre_shock', options=[0, 1])
-area_of_burn = st.number_input('area_of_shock', min_value=20, max_value=100, step=1)
-three = st.number_input('Ⅲ', min_value=0, max_value=80, step=1)
-inhalation_damage = st.selectbox('inhalation_damage', options=[0, 1])
-ALB = st.number_input('ALB', min_value=5.0, max_value=50.0, step=0.1)
+TBIL = st.number_input('TBIL (μmol/L)', min_value=1.0, max_value=100.0, step=0.1)
+SCr = st.number_input('SCr (μmol/L)', min_value=0.0, max_value=300.0, step=0.1)
+pre_shock = st.selectbox('pre_shock', options=["Yes", "No"])
+area_of_burn = st.number_input('area_of_shock (%)', min_value=10, max_value=100, step=1)
+three = st.number_input('Ⅲ (%)', min_value=0, max_value=80, step=1)
+inhalation_damage = st.selectbox('inhalation_damage', options=["Yes", "No"])
+ALB = st.number_input('ALB (g/L)', min_value=5.0, max_value=50.0, step=0.1)
+
+if new_onset_shock == "Yes":
+    new_onset_shock = 1
+else:
+    new_onset_shock = 0
+if MDR == "Yes":
+    MDR = 1
+else:
+    MDR = 0
+if pre_shock == "Yes":
+    pre_shock = 1
+else:
+    pre_shock = 0
+if inhalation_damage == "Yes":
+    inhalation_damage = 1
+else:
+    inhalation_damage = 0
 
 # 定义应用程序行为
 if st.button('Predict'):
